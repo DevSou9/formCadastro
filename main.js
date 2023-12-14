11a3dca8c7cf4c4a730e41d12f6880bdd000ca65
W Souza <dev9.ws@gmail.com>
2023-12-14 11:37:45 -0300

    feature/validarNome

--- main.js
+++ main.js
@@ -2,13 +2,43 @@ const form = document.querySelector('form');
 const nomeCadastro = document.getElementById('nome');
 const telfoneCadastro = document.getElementById('telefone');
 const bodyTable = document.querySelector('tbody');
+const footTable = document.querySelector('tfoot')
+const pNameInvalido = document.getElementById('msg-nome-invalido')
 let linhas = '';
+let count = 0;
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
-    let linha = `<tr><td>${nomeCadastro.value}</td></tr><tr><td>${telfoneCadastro.value}</td></tr>`;
-    linhas += linha;    
-
-    bodyTable.innerHTML = linhas;
+   
+    addRows();
+    cleanForm();
 })
+
+function addRows(){
+    let nome = nomeCadastro.value;
+    console.log(`Entrou no addRows:`);
+    if(validarNome(nome)){
+        let telefone = telfoneCadastro.value;
+        let linha = `<tr><td>${nome}</td></tr><tr><td>${telefone}</td></tr>`;
+        linhas += linha;
+        bodyTable.innerHTML = linhas;
+        count++;
+        footTable.innerHTML = `Número de cadastrados: ${count}`
+    }
+    
+}
+
+function cleanForm(){
+    nomeCadastro.value = '';
+    telfoneCadastro.value = '';
+}
+
+function validarNome(nome){
+   console.log(`Entrou no valida nome`);
+    let nomeArray = nome.split(' ')
+    if(nomeArray.length < 2){
+        pNameInvalido.innerHTML = "Necessário inserir nome completo!"
+        return false;
+    }
+    return true;
+}
